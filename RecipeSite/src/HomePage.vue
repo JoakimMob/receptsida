34104bff57081153244a15fa115841df808b6351
Ismail El Kawwash <ismelk91@users.noreply.github.com>
2023-09-28 21:31:03 +0200

    Ismail
    cleaned up project

---
+++ RecipeSite/src/HomePage.vue
@@ -0,0 +1,10 @@
+<script setup>
+// import categories from './components/categories.vue';
+</script>
+
+<template>
+  <h1>Välkommen till vår recept sida</h1>
+</template>
+
+<style scoped>
+</style>
--- RecipeSite/src/main.js
+++ RecipeSite/src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import HomePage from './HomePage.vue'
 
-createApp(App).mount('#app')
+createApp(HomePage).mount('#app')
