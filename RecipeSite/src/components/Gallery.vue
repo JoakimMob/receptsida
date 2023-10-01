39a6a84764839d53ea1678062987299aaaf0fc50
Ismail El Kawwash <ismelk91@users.noreply.github.com>
2023-10-01 15:36:17 +0200

    Added recept-gallery on homepage. Deleted categories.html, recipes.html files.

---
+++ RecipeSite/src/components/Gallery.vue
@@ -0,0 +1,56 @@
+<script >
+
+export default {
+    data() {
+        return {
+            recipes: [],
+        }
+    },
+    created() {
+        fetch('https://jau22-recept-grupp5-1bixsi9xz341.reky.se/recipes')
+            .then(response => response.json())
+            .then(data => {
+                this.recipes = data
+                console.log(data)
+            })
+    },
+}
+</script>
+
+<template>
+    <div class="gallery-list">
+        <li class="card" v-for="recipe in recipes" :key="recipe.id">
+            <img :src="recipe.imageUrl">
+            <div>{{ recipe.title }}</div>
+        </li>
+    </div>
+</template>
+
+
+<style>
+.gallery-list {
+    padding: 100px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 1rem;
+}
+
+.card {
+    background-color: #fff;
+    list-style-type: none;
+    border: 1px solid black;
+    min-width: 250px;
+    max-width: 350px;
+    max-height: 300px;
+    border-radius: 3px;
+    text-align: center;
+    font-size: 1.5rem;
+}
+
+.card  img{
+    margin:8px auto;
+    height: 220px;
+    width: 95%;
+}
+
+</style>
