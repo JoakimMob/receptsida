29470f2f14a625cd57e4c4f1ba84350acc191709
Hanna Friberg <friberg.hanna@outlook.com>
2023-10-11 18:56:29 +0200

    New CSS NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,21 +1,37 @@
 <template>
-    <div>
+    <div class="sidebar close">
+        <div class="logo-details">
+            <span id=icon class="material-symbols-outlined">dinner_dining</span>
+            <span class="logo_name">Italian cuisine</span>
+        </div>
         
-        <ul>  
-            <h2>
-            <router-link to="/">Home</router-link>
-            </h2>
-            <h3>{{ message }}</h3>
-
-            <li v-for="item in apiData" :key="item.id">
-                <router-link :to="`/category/${item.name}`" @click="makeBold(item.name)">
-                    <Categories :name="item.name" :count="item.count" />
-                </router-link>
+        <ul class="nav-links">  
+            <li>
+            <router-link to="/">
+                <i class='bx bx-home-heart'></i>
+                <span class="link_name">Startsida</span>
+            </router-link>
             </li>
-        </ul>
+
+            <li>
+                <div class="icon-link">
+                    <a href="#">
+                        <i class='bx bx-category'></i>
+                        <span class="link_name">Kategorier</span>
+                    </a>
+                </div>
+                <ul class="sub-menu">
+                    <li v-for="item in apiData" :key="item.id">
+                        <router-link :to="`/category/${item.name}`" @click="makeBold(item.name)">
+                            <Categories :name="item.name" :count="item.count" />
+                        </router-link>
+                    </li>
+                </ul>
+            </li>
+        </ul>   
     </div>
 </template>
-
+ 
 <script>
 import { RouterLink } from 'vue-router';
 import Categories from './Categories.vue';
@@ -23,7 +39,6 @@ import Categories from './Categories.vue';
 export default {
     data() {
         return {
-            message: 'Kategorier:',
             apiData: [],
             categories: document.getElementsByClassName("categories")
         };
@@ -48,35 +63,119 @@ export default {
     components: { Categories, RouterLink }
 }
 </script>
-
-
-
-<style scoped>
-:root {
-    grid-column: 1/3;
-    grid-row: 2;
-}
-
-h3 {
-    color: rgb(12, 12, 12);
-    font-style: italic;
-    font-size: medium;
-
-}
-ul {
-    padding: 70px 50px;
-    background-color: #9bb979;
-    color: white;
-    position: absolute;
+ 
+<style>
+.sidebar {
+    position: fixed;
     top: 0;
-    left: -300px;
-    width: 200px;
-    height: 100vh;
-    z-index: 1; 
     left: 0;
+    height: 100%;
+    width: 260px;
+    background: #11101d;
 }
+
+.sidebar .logo-details {
+    height: 60px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+}
+
+#icon {
+    font-size: 50px;
+    color: white;
+    height: 50px;
+    min-width: 78px;
+    text-align: center;
+    line-height: 50px;
+}
+
+.sidebar .logo-details .logo_name {
+    font-size: 22px;
+    color: white;
+    font-weight: 600;
+
+}
+
+.sidebar .nav-links {
+    height: 100%;
+    padding-top: 30px;
+}
+
+.sidebar .nav-links li .icon-link {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.sidebar .nav-links li {
+    position: relative;
+    list-style: none;
+    transition: all 0.4s ease;
+}
+
+.sidebar .nav-links li:hover {
+    background: #1d1b31;
+}
+
+.sidebar .nav-links li i {
+    height: 50px;
+    min-width: 78px;
+    text-align: center;
+    line-height: 50px;
+    color: #fff;
+    font-size: 30px;
+}
+
+.sidebar .nav-links li a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+}
+
+.sidebar .nav-links li a .link_name {
+    font-size: 18px;
+    font-weight: 400;
+    color: white;
+}
+
+.sidebar .nav-links li .sub-menu {
+    padding: 6px 6px 14px 80px;
+    margin-top: -10px;
+    background-color: #1d1b31;
+}
+
+.sidebar .nav-links li .sub-menu a {
+    color: white;
+    font-size: 15px;
+    padding: 5px 0;
+    white-space: nowrap;
+    opacity: 0.6;
+    transition: all 0.3s ease;
+}
+
+.sidebar .nav-links li .sub-menu a:hover {
+    opacity: 1;
+}
+
+/* .sidebar.close .nav-links li .sub-menu{
+    position: absolute;
+    left: 100%;
+    top: 0;
+    margin-top: 0;
+    padding: 10px 20px;
+    border-radius: 0 6px 6px 0;
+    transition: all 0.4s ease;
+}
+
+.sidebar .nav-links li .sub-menu .link_name{
+    display: none;
+
+}
+
+.sidebar.close .nav-links li .sub-menu .link_name{
+    font-size: 18px;
+    opacity: 1;
+    display: block;
+} */
 </style>
-
-
-
-
